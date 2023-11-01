51c1f0f49df201cd906d8eb945b6f81b4693cb35
Raadsl <83394320+Raadsl@users.noreply.github.com>
2023-11-01 21:10:10 +0000

    Improved the UX and UI very much for all pages.

--- script.js
+++ script.js
@@ -1,136 +1,172 @@
-function copy() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var inputValue2 = document.getElementById("myInput2").value;
-  const t1 = document.getElementById("errormsg");
-  const count = parseInt(inputValue2) 
-  if (inputValue2 != count.toString()) {
-    alert(`ERROR. Please give a NUMBER of websites you want to open. You didnt enter a valid number:(`)
-  }
-  else {
-  //navigator.clipboard.writeText(`https://website-spammer.raadsel.repl.co/customurl.html?url=${inputValue}&times=${count}`);
-  navigator.clipboard.writeText(`https://website-spammer.raadsel.repl.co/customurl.html?url=${inputValue}&times=${count}`).catch(function(error) {
-    alert("An error has occurred: "+error);
-    document.getElementById("copied").innerHTML = error
-    throw new Error(error);
-}).then(function(x) {
-      alert(`Copied URL to open "${inputValue}", "${inputValue2}" times once it loads!`)
-      document.getElementById("copied").innerHTML = `Copied URL to open ${inputValue}, ${inputValue2} times!`;
-    });
-  //document.getElementById("copied").innerHTML = `Copied URL to open ${inputValue}, ${inputValue2} times!`;
-  //alert(`Copied URL to open "${inputValue}", "${inputValue2}" times once it loads!`)
-  }
-  t1.innerText = `Copied URL to clipboard!`;
-  if(inputValue === '' && inputValue2 === '') {
-    const title = document.getElementById("errormsg");
-    title.innerText = `You must enter a website URL`;
-    alert('You must enter the count of the websites you want to open')
-  }
-  else {
-    if (inputValue === '') {
+function displayErrorModal(message) {
+  const modal = document.getElementById("Modal");
+  const closeButton = document.getElementsByClassName("close")[0];
 
-      const title = document.getElementById("errormsg");
-      title.innerText = `You must enter a website URL`;
-      alert("You must enter a website URL");
-      
+  document.getElementById("content").textContent = message;
+
+  modal.style.display = "block";
+
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
+}
+
+function copyToClipboard(text) {
+  if (navigator.clipboard && window.isSecureContext) {
+    return navigator.clipboard.writeText(text);
   } else {
-      if(inputValue2 === '') {
-
-        const title = document.getElementById("errormsg");
-        title.innerText = `You must enter the count of the websites you want to open`;
-        alert("You must enter the count of the websites you want to open")
-        
-    }
-    
-  }
-  }
-
-
-  
-}
-
-
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var inputValue2 = document.getElementById("myInput2").value;
-  const t1 = document.getElementById("errormsg");
-  const count = parseInt(inputValue2) 
-  if (inputValue2 != count.toString()) {
-    alert(`ERROR. Please give a NUMBER of websites you want to open. You didnt enter a valid number:(`)
-  }
-  else {
-  t1.innerText = `Opening: "${count}" Websites!`;
-  for (let i = 0; i < count; i++) {
-    window.open(inputValue)
-  }
-  t1.innerText = `Done opening ${count} websites! If only 1 tab opened for you, you may need to give permission for this website to open more! If I didnt open any websites, please check you entered a number at the second input bar!`;
-  if(inputValue === '' && inputValue2 === '') {
-    const title = document.getElementById("errormsg");
-    title.innerText = `You must enter a website URL`;
-    alert('You must enter the count of the websites you want to open')
-  }
-  else {
-    if (inputValue === '') {
-
-      const title = document.getElementById("errormsg");
-      title.innerText = `You must enter a website URL`;
-      alert("You must enter a website URL");
-      
-  } else {
-      if(inputValue2 === '') {
-
-        const title = document.getElementById("errormsg");
-        title.innerText = `You must enter the count of the websites you want to open`;
-        alert("You must enter the count of the websites you want to open")
-        
-    }
-    
-  }
-  }
-
-
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+    let input = document.createElement('textarea');
+    input.value = text;
+    document.body.appendChild(input);
+    input.focus();
+    input.select();
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    return Promise.resolve();
   }
 }
 
+function displayPromptModal() {
+  const modal = document.getElementById("Modal");
+  const closeButton = document.getElementsByClassName("close")[0];
 
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-  let expires = "expires="+d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+  modal.style.display = "block";
+
+  closeButton.onclick = function () {
+    modal.style.display = "none";
+  };
+
+  window.onclick = function (event) {
+    if (event.target === modal) {
+      modal.style.display = "none";
+    }
+  };
 }
 
-function getCookie(cname) {
-  let name = cname + "=";
-  let ca = document.cookie.split(';');
-  for(let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == ' ') {
-      c = c.substring(1);
+function setCookie(cookieName, cookieValue, expirationDays) {
+  const date = new Date();
+  date.setTime(date.getTime() + (expirationDays * 24 * 60 * 60 * 1000));
+  const expires = "expires=" + date.toUTCString();
+  document.cookie = cookieName + "=" + cookieValue + ";" + expires + ";path=/";
+}
+
+function getCookie(cookieName) {
+  const name = cookieName + "=";
+  const cookieArray = document.cookie.split(';');
+
+  for (let i = 0; i < cookieArray.length; i++) {
+    let cookie = cookieArray[i];
+    while (cookie.charAt(0) === ' ') {
+      cookie = cookie.substring(1);
     }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
+    if (cookie.indexOf(name) === 0) {
+      return cookie.substring(name.length, cookie.length);
     }
   }
   return "";
 }
 
-//request scirpt
-
-
-function httpGet(theUrl) {
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "GET", theUrl, false ); // false for synchronous request
-    xmlHttp.send( null );
-    return xmlHttp.responseText;
+function checkCookie() {
+const userCheck = getCookie("user_visit_check");
+if (userCheck !== "") {
+  console.log("Welcome back!");
+} else {
+  displayPromptModal();
+  setCookie("user_visit_check", "Confirmed", 14);
 }
-console.log(httpGet("/LICENSE"))
-console.log("Made by Jorik aka Raadsel")
+}
+
+checkCookie();
+
+function copyUrl() {
+  const urlValue = document.getElementById("url").value;
+  const countValue = document.getElementById("count").value;
+  const errorMessage = document.getElementById("status");
+  const count = parseInt(countValue);
+
+  if (countValue !== count.toString()) {
+    displayErrorModal("Error, please enter a valid number for the count of websites you want to open.");
+  } else {
+    navigator.clipboard.writeText(`https://raadsl.github.io/website-spammer/customurl?url=${urlValue}&times=${count}`)
+      .catch(function(error) {
+        displayErrorModal("An error has occurred: " + error);
+        throw new Error(error);
+      })
+      .then(function() {
+        errorMessage.innerText = `Copied URL to open ${urlValue}, ${countValue} times once it loads!`;
+      });
+  }
+
+  errorMessage.innerText = "Copied URL to clipboard!";
+
+  if (urlValue === '' && countValue === '') {
+    errorMessage.innerText = "You must enter a website URL and the count of websites you want to open.";
+    displayErrorModal('You must enter a website URL and the count of websites you want to open.');
+  } else {
+    if (urlValue === '') {
+      errorMessage.innerText = "You must enter a website URL.";
+      displayErrorModal("You must enter a website URL.");
+    } else if (countValue === '') {
+      errorMessage.innerText = "You must enter the count of websites you want to open.";
+      displayErrorModal("You must enter the count of websites you want to open.");
+    }
+  }
+}
+
+function openMultipleTabs() {
+const url = document.getElementById("url").value;
+let count = parseInt(document.getElementById("count").value);
+const statusMessage = document.getElementById("status");
+
+if (isNaN(count)) {
+  displayErrorModal("Error, please enter a valid number for the count of websites you want to open.");
+} else {
+  statusMessage.innerText = `Opening: "${count}" Websites!`;
+  for (let i = 0; i < count; i++) {
+    window.open(url);
+  }
+
+  statusMessage.innerText = `Done opening ${count} websites! If only one tab opened for you, you may need to give permission for this website to open more!`;
+
+  if (url === '' && isNaN(count)) {
+    statusMessage.innerText = "You must enter a website URL and the count of websites you want to open.";
+    displayErrorModal('You must enter a website URL and the count of websites you want to open.');
+  } else {
+    if (url === '') {
+      statusMessage.innerText = "You must enter a website URL.";
+      displayErrorModal("You must enter a website URL.");
+    } else if (isNaN(count)) {
+      statusMessage.innerText = "You must enter the count of websites you want to open.";
+      displayErrorModal("You must enter the count of websites you want to open.");
+    }
+  }
+}
+}
+
+document.getElementById("copyBtn").addEventListener('click', copyUrl);
+document.getElementById("openBtn").addEventListener('click', openMultipleTabs);
+
+//virus and auto spammer
+
+document.getElementById("virus-copy").addEventListener("click", function() {
+  copyToClipboard("https://raadsl.github.io/website-spammer/virusspam")
+  document.getElementById("virus-copy").innerText = `URL copied`
+  setTimeout(function() {document.getElementById("virus-copy").innerText = `Copy URL`}, 1100);
+});
+document.getElementById("virus-open").addEventListener("click", function() {
+  window.open("https://raadsl.github.io/website-spammer/virusspam");
+});
+document.getElementById("auto-copy").addEventListener("click", function() {
+  copyToClipboard("https://raadsl.github.io/website-spammer/autospam")
+  document.getElementById("auto-copy").innerText = `URL copied`
+  setTimeout(function() {document.getElementById("auto-copy").innerText = `Copy URL`}, 1100);
+});
+document.getElementById("auto-open").addEventListener("click", function() {
+  window.open("https://raadsl.github.io/website-spammer/autospam");
+});
